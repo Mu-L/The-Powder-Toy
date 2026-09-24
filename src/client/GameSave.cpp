Mu-L/The-Powder-Toy

commit 51f1afbf863333c27795e06a5bffb6c2ed28d887

Fix crashes when copying in saves with many authors
By reverting a good portion of 9d1412019fb1, where I got too greedy and *in addition to* replacing the entire BSON implementation, I also replaced author info, which used to be a Json::Value from jsoncpp, with Bson, which is from the new BSON implementation. I did this because I really wanted to get rid of Json::Value in Client and other places where it had no business being; jacob chose it as the way to pass recursive data around because the old JSON implementation did not offer any such data structure.

Little did I know that the code that deals with author info is extremely reliant on the kind of sloppy incorrect JSON subobject access that jsoncpp allows, for some reason. This includes allowing taking the sizes of strings as if they are arrays, interpreting objects as integers, and in general just not checking subobject types at all and treating them as if they are the types of objects that you need.

My BSON code naturally crashes instead of papering over blatant coding mistakes like that, and since 29ae4d665440, it does this even more spectacularly than before, since instead of throwing very obvious std::bad_variant_accesses, it just invokes UB. This is arguably not ideal, and I may fix this later, but it would not fix the crashes this commit addresses.

The solution is to revert the parts of the aforementioned commit that replace Json::Values with Bsons. This transmutes the burden of checking all the sloppy code that originally used Json::Values into the burden of merely rewriting old BSON API usage into new BSON API usage in ConvertBsonToJson and ConvertJsonToBson. I should totally have done this in the first place.

This brings Json::Value back into Client, which is annoying, but it will go away when we flatten author info sometime in the future, which is something I can live with.

src/client/GameSave.cpp
@@ -21,9 +21,8 @@ static_assert(!ALLOW_FAKE_NEWER_VERSION || nextVersion >= currentVersion);
 
 constexpr auto effectiveVersion = ALLOW_FAKE_NEWER_VERSION ? nextVersion : currentVersion;
 
-static void TrimAuthorsIn(Bson &b, int depth);
-static std::set<int> GetNestedSaveIDs(const Bson &j);
-static void TrimAuthorsOut(Bson &b, int depth);
+static void ConvertJsonToBson(Bson &b, Json::Value j, int depth = 0);
+static void ConvertBsonToJson(const Bson &b, Json::Value *j, int depth = 0);
 
 GameSave::GameSave(Vec2<int> newBlockSize)
 {
@@ -765,8 +764,10 @@ void GameSave::readOPS(const std::vector<char> &data)
 	}
 	if (auto *authorsNode = getIfType(b, "authors", Bson::Type::objectValue); authorsNode && wantAuthors)
 	{
-		authors = *authorsNode;
-		TrimAuthorsIn(authors, 0);
+		// we need to clear authors because the save may be read multiple times in the stamp browser (loading and rendering twice)
+		// seems inefficient ...
+		authors.clear();
+		ConvertBsonToJson(*authorsNode, &authors);
 	}
 
 	auto paletteRemap = [this](auto maxVersion, ByteString from, ByteString to) {
@@ -2696,10 +2697,10 @@ std::pair<bool, std::vector<char>> GameSave::serialiseOPS() const
 			}
 		}
 	}
-	if (authors.GetSize())
+	if (authors.size())
 	{
-		auto &authorsNode = (b["authors"] = authors);
-		TrimAuthorsOut(authorsNode, 0);
+		auto &authorsNode = (b["authors"] = Bson::Type::objectValue);
+		ConvertJsonToBson(authorsNode, authors);
 	}
 
 	std::vector<char> finalData;
@@ -2750,64 +2751,64 @@ std::pair<bool, std::vector<char>> GameSave::serialiseOPS() const
 	return { fakeFromNewerVersion, outputData };
 }
 
-static void TrimAuthorsIn(Bson &b, int depth)
+static void ConvertBsonToJson(const Bson &b, Json::Value *j, int depth)
 {
-	for (auto &[ key, child ] : b.As<Bson::Object>())
+	for (auto &[ key, value ] : b.As<Bson::Object>())
 	{
-		if (child.Is<Bson::Array>())
+		if (value.GetType() == Bson::Type::stringValue)
+			(*j)[key] = value.As<Bson::String>();
+		else if (value.GetType() == Bson::Type::boolValue)
+			(*j)[key] = value.As<Bson::Bool>();
+		else if (value.GetType() == Bson::Type::int32Value)
+			(*j)[key] = value.As<Bson::Int32>();
+		else if (value.GetType() == Bson::Type::int64Value)
+			(*j)[key] = (Json::Value::UInt64)value.As<Bson::Int64>();
+		else if (value.GetType() == Bson::Type::arrayValue && depth < 5)
 		{
-			Bson newChild(Bson::Type::arrayValue);
-			if (depth < 5)
+			int length = 0, length2 = 0;
+			for (auto &item : value.As<Bson::Array>())
 			{
-				int length = 0, length2 = 0;
-				for (auto &link : child.As<Bson::Array>())
+				if (item.GetType() == Bson::Type::objectValue) // this used to check whether the """key""" (funny because this is an array) was "part"
 				{
-					if (link.Is<Bson::Object>())
-					{
-						auto &newLink = newChild.Append(link);
-						TrimAuthorsIn(newLink, depth + 1);
-						length++;
-					}
-					else if (link.Is<int32_t>())
-					{
-						newChild.Append(link.As<int32_t>());
-					}
-					length2++;
-					if (length > (40 / ((depth + 1) * (depth + 1))) || length2 > 50)
-					{
-						break;
-					}
+					Json::Value tempPart;
+					ConvertBsonToJson(item, &tempPart, depth + 1);
+					(*j)["links"].append(tempPart);
+					length++;
 				}
+				else if (item.GetType() == Bson::Type::int32Value) // this used to check whether the """key""" (funny because this is an array) was "saveID"
+				{
+					(*j)["links"].append(item.As<Bson::Int32>());
+				}
+				length2++;
+				if (length > (int)(40 / ((depth+1) * (depth+1))) || length2 > 50)
+					break;
 			}
-			child = std::move(newChild);
 		}
 	}
 }
 
-static std::set<int> GetNestedSaveIDs(const Bson &j)
+std::set<int> GetNestedSaveIDs(Json::Value j)
 {
-	std::set<int> saveIDs;
-	for (auto &[ key, member ] : j.As<Bson::Object>())
+	Json::Value::Members members = j.getMemberNames();
+	std::set<int> saveIDs = std::set<int>();
+	for (Json::Value::Members::iterator iter = members.begin(), end = members.end(); iter != end; ++iter)
 	{
-		if (member.Is<int32_t>())
+		ByteString member = *iter;
+		if (member == "id" && j[member].isInt())
+			saveIDs.insert(j[member].asInt());
+		else if (j[member].isArray())
 		{
-			saveIDs.insert(member.As<int32_t>());
-		}
-		else if (member.Is<Bson::Array>())
-		{
-			for (auto &link : member.As<Bson::Array>())
+			for (Json::Value::ArrayIndex i = 0; i < j[member].size(); i++)
 			{
 				// only supports objects and ints here because that is all we need
-				if (link.Is<int32_t>())
+				if (j[member][i].isInt())
 				{
-					saveIDs.insert(link.As<int32_t>());
+					saveIDs.insert(j[member][i].asInt());
 					continue;
 				}
-				if (!link.Is<Bson::Object>())
-				{
+				if (!j[member][i].isObject())
 					continue;
-				}
-				auto nestedSaveIDs = GetNestedSaveIDs(link);
+				std::set<int> nestedSaveIDs = GetNestedSaveIDs(j[member][i]);
 				saveIDs.insert(nestedSaveIDs.begin(), nestedSaveIDs.end());
 			}
 		}
@@ -2816,44 +2817,51 @@ static std::set<int> GetNestedSaveIDs(const Bson &j)
 }
 
 // converts a json object to bson
-static void TrimAuthorsOut(Bson &b, int depth)
+static void ConvertJsonToBson(Bson &b, Json::Value j, int depth)
 {
-	for (auto &[ key, member ] : b.As<Bson::Object>())
+	Json::Value::Members members = j.getMemberNames();
+	for (Json::Value::Members::iterator iter = members.begin(), end = members.end(); iter != end; ++iter)
 	{
-		if (member.Is<Bson::Array>())
+		ByteString member = *iter;
+		if (j[member].isString())
+			b[member.c_str()] = j[member].asCString();
+		else if (j[member].isBool())
+			b[member.c_str()] = j[member].asBool();
+		else if (j[member].type() == Json::intValue)
+			b[member.c_str()] = j[member].asInt();
+		else if (j[member].type() == Json::uintValue)
+			b[member.c_str()] = j[member].asInt64();
+		else if (j[member].isArray())
 		{
-			Bson newChild(Bson::Type::arrayValue);
-			std::set<int> saveIDs;
+			auto &array = b.Append(Bson::Type::arrayValue);
+			std::set<int> saveIDs = std::set<int>();
 			int length = 0;
-			for (auto &link : member.As<Bson::Array>())
+			for (Json::Value::ArrayIndex i = 0; i < j[member].size(); i++)
 			{
 				// only supports objects and ints here because that is all we need
-				if (link.Is<int32_t>())
+				if (j[member][i].isInt())
 				{
-					saveIDs.insert(link.As<int32_t>());
+					saveIDs.insert(j[member][i].asInt());
 					continue;
 				}
-				if (!link.Is<Bson::Object>())
-				{
+				if (!j[member][i].isObject())
 					continue;
-				}
-				if (depth > 4 || length > 40 / ((depth + 1) * (depth + 1)))
+				if (depth > 4 || length > (int)(40 / ((depth+1) * (depth+1))))
 				{
-					std::set<int> nestedSaveIDs = GetNestedSaveIDs(link);
+					std::set<int> nestedSaveIDs = GetNestedSaveIDs(j[member][i]);
 					saveIDs.insert(nestedSaveIDs.begin(), nestedSaveIDs.end());
 				}
 				else
 				{
-					auto &newLink = newChild.Append(link);
-					TrimAuthorsOut(newLink, depth + 1);
+					auto &part = array.Append(Bson::Type::objectValue);
+					ConvertJsonToBson(part, j[member][i], depth+1);
 				}
 				length++;
 			}
-			for (auto id : saveIDs)
+			for (std::set<int>::iterator iter = saveIDs.begin(), end = saveIDs.end(); iter != end; ++iter)
 			{
-				newChild.Append(id);
+				array.Append(*iter);
 			}
-			member = std::move(newChild);
 		}
 	}
 }

src/client/Client.cpp
@@ -361,15 +361,15 @@ ByteString Client::AddStamp(std::unique_ptr<GameSave> saveData)
 
 	Platform::MakeDirectory(STAMPS_DIR);
 
-	Bson stampInfo;
+	Json::Value stampInfo;
 	stampInfo["type"] = "stamp";
 	stampInfo["username"] = authUser ? authUser->Username : ByteString("");
 	stampInfo["name"] = filename;
-	stampInfo["date"] = int64_t(now);
-	if (authors.GetSize() != 0)
+	stampInfo["date"] = Json::Value::UInt64(now);
+	if (authors.size() != 0)
 	{
 		// This is a stamp, always append full authorship info (even if same user)
-		stampInfo["links"].Append(Client::Ref().authors);
+		stampInfo["links"].append(Client::Ref().authors);
 	}
 	saveData->authors = stampInfo;
 
@@ -474,28 +474,27 @@ std::unique_ptr<SaveFile> Client::LoadSaveFile(ByteString filename)
 
 // stamp-specific wrapper for MergeAuthorInfo
 // also used for clipboard and lua stamps
-void Client::MergeStampAuthorInfo(const Bson &stampAuthors)
+void Client::MergeStampAuthorInfo(Json::Value stampAuthors)
 {
-	if (stampAuthors.GetSize())
+	if (stampAuthors.size())
 	{
 		// when loading stamp/clipboard, only append info to authorship info (since we aren't replacing the save)
 		// unless there is nothing loaded currently, then set authors directly
-		if (authors.GetSize())
+		if (authors.size())
 		{
-			auto links = stampAuthors.Get("links", Bson::Type::arrayValue);
 			// Don't add if it's exactly the same
-			if (links.GetSize() == 1 && links[0] == Client::Ref().authors)
+			if (stampAuthors["links"].size() == 1 && stampAuthors["links"][0] == Client::Ref().authors)
 				return;
-			if (authors["username"] != stampAuthors.Get("username", ByteString("")))
+			if (authors["username"] != stampAuthors["username"])
 			{
 				// 2nd arg of MergeAuthorInfo needs to be an array
-				Bson toAdd;
-				toAdd.Append(stampAuthors);
+				Json::Value toAdd;
+				toAdd.append(stampAuthors);
 				MergeAuthorInfo(toAdd);
 			}
-			else if (links.GetSize())
+			else if (stampAuthors["links"].size())
 			{
-				MergeAuthorInfo(links);
+				MergeAuthorInfo(stampAuthors["links"]);
 			}
 		}
 		else
@@ -504,38 +503,38 @@ void Client::MergeStampAuthorInfo(const Bson &stampAuthors)
 }
 
 // linksToAdd is an array (NOT an object) of links to add to authors["links"]
-void Client::MergeAuthorInfo(const Bson &linksToAdd)
+void Client::MergeAuthorInfo(Json::Value linksToAdd)
 {
-	for (auto &link : linksToAdd.As<Bson::Array>())
+	for (Json::Value::ArrayIndex i = 0; i < linksToAdd.size(); i++)
 	{
 		// link is the same exact json we have open, don't do anything
-		if (link == authors)
+		if (linksToAdd[i] == authors)
 			return;
 
 		bool hasLink = false;
-		for (Json::Value::ArrayIndex j = 0; j < authors["links"].GetSize(); j++)
+		for (Json::Value::ArrayIndex j = 0; j < authors["links"].size(); j++)
 		{
 			// check everything in authors["links"] to see if it's the same json as what we are already adding
-			if (authors["links"][j] == link)
+			if (authors["links"][j] == linksToAdd[i])
 				hasLink = true;
 		}
 		if (!hasLink)
-			authors["links"].Append(link);
+			authors["links"].append(linksToAdd[i]);
 	}
 }
 
 // load current authors information into a json value (when saving everything: stamps, clipboard, local saves, and online saves)
-void Client::SaveAuthorInfo(Bson &saveInto) const
+void Client::SaveAuthorInfo(Json::Value *saveInto)
 {
-	if (authors.GetSize() != 0)
+	if (authors.size() != 0)
 	{
 		// Different username? Save full original save info
-		if (authors.Get("username", ByteString("")) != saveInto["username"])
-			saveInto["links"].Append(authors);
+		if (authors["username"] != (*saveInto)["username"])
+			(*saveInto)["links"].append(authors);
 		// This is probalby the same save
 		// Don't append another layer of links, just keep existing links
-		else if (auto links = authors.Get("links", Bson::Type::arrayValue); links.GetSize())
-			saveInto["links"] = links;
+		else if (authors["links"].size())
+			(*saveInto)["links"] = authors["links"];
 	}
 }
 

src/client/Client.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "common/Bson.h"
 #include "common/String.h"
 #include "common/ExplicitSingleton.h"
 #include "StartupInfo.h"
@@ -9,6 +8,7 @@
 #include <list>
 #include <memory>
 #include <optional>
+#include <json/json.h>
 
 class SaveInfo;
 class SaveFile;
@@ -59,7 +59,7 @@ private:
 	void notifyNewNotification(ServerNotification notification);
 
 	// Save stealing info
-	Bson authors;
+	Json::Value authors;
 
 	std::unique_ptr<Prefs> stamps;
 	void MigrateStampsDef();
@@ -76,13 +76,13 @@ public:
 	std::vector<ClientListener*> listeners;
 
 	// Save stealing info
-	void MergeStampAuthorInfo(const Bson &linksToAdd);
-	void MergeAuthorInfo(const Bson &linksToAdd);
-	void OverwriteAuthorInfo(Bson overwrite) { authors = std::move(overwrite); }
-	const Bson &GetAuthorInfo() const { return authors; }
-	void SaveAuthorInfo(Bson &saveInto) const;
-	void ClearAuthorInfo() { authors = Bson{}; }
-	bool IsAuthorsEmpty() const { return authors.GetSize() == 0; }
+	void MergeStampAuthorInfo(Json::Value linksToAdd);
+	void MergeAuthorInfo(Json::Value linksToAdd);
+	void OverwriteAuthorInfo(Json::Value overwrite) { authors = overwrite; }
+	Json::Value GetAuthorInfo() { return authors; }
+	void SaveAuthorInfo(Json::Value *saveInto);
+	void ClearAuthorInfo() { authors.clear(); }
+	bool IsAuthorsEmpty() { return authors.size() == 0; }
 
 	std::optional<UpdateInfo> GetUpdateInfo();
 

src/client/GameSave.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "common/Bson.h"
 #include "common/Plane.h"
 #include "common/String.h"
 #include "common/tpt-rand.h"
@@ -13,6 +12,7 @@
 #include "SimulationConfig.h"
 #include <vector>
 #include <array>
+#include <json/json.h>
 
 struct sign;
 struct Particle;
@@ -129,7 +129,7 @@ public:
 	std::vector<PaletteItem> palette;
 
 	// author information
-	Bson authors;
+	Json::Value authors;
 
 	int pmapbits = 8; // default to 8 bits for older saves
 

src/gui/game/Favorite.cpp
@@ -1,5 +1,6 @@
 #include "Favorite.h"
 #include "prefs/GlobalPrefs.h"
+#include <json/json.h>
 #include <algorithm>
 
 Favorite::Favorite():

src/gui/game/GameController.cpp
@@ -496,12 +496,12 @@ void GameController::CopyRegion(ui::Point point1, ui::Point point2)
 	auto newSave = gameModel->GetSimulation()->Save(gameModel->GetIncludePressure() != gameView->ShiftBehaviour(), SaneSaveRect(point1, point2));
 	if(newSave)
 	{
-		Bson clipboardInfo;
+		Json::Value clipboardInfo;
 		clipboardInfo["type"] = "clipboard";
 		auto user = Client::Ref().GetAuthUser();
 		clipboardInfo["username"] = user ? user->Username : ByteString("");
-		clipboardInfo["date"] = int64_t(time(nullptr));
-		Client::Ref().SaveAuthorInfo(clipboardInfo);
+		clipboardInfo["date"] = (Json::Value::UInt64)time(nullptr);
+		Client::Ref().SaveAuthorInfo(&clipboardInfo);
 		newSave->authors = clipboardInfo;
 
 		newSave->paused = gameModel->GetPaused();
@@ -1272,13 +1272,13 @@ void GameController::OpenLocalSaveWindow(bool asCurrent)
 		}
 		else if (gameModel->GetSaveFile())
 		{
-			Bson localSaveInfo;
+			Json::Value localSaveInfo;
 			localSaveInfo["type"] = "localsave";
 			auto user = Client::Ref().GetAuthUser();
 			localSaveInfo["username"] = user ? user->Username : ByteString("");
 			localSaveInfo["title"] = gameModel->GetSaveFile()->GetName();
-			localSaveInfo["date"] = int64_t(time(nullptr));
-			Client::Ref().SaveAuthorInfo(localSaveInfo);
+			localSaveInfo["date"] = (Json::Value::UInt64)time(nullptr);
+			Client::Ref().SaveAuthorInfo(&localSaveInfo);
 			gameSave->authors = localSaveInfo;
 
 			Platform::MakeDirectory(LOCAL_SAVE_DIR);

src/gui/game/GameModel.cpp
@@ -872,7 +872,7 @@ void GameModel::SetSave(std::unique_ptr<SaveInfo> newSave, bool invertIncludePre
 		sim->Load(saveData, !invertIncludePressure, { 0, 0 });
 		// This save was created before logging existed
 		// Add in the correct info
-		if (saveData->authors.GetSize() == 0)
+		if (saveData->authors.size() == 0)
 		{
 			auto gameSave = currentSave.saveInfo->TakeGameSave();
 			gameSave->authors["type"] = "save";
@@ -881,12 +881,12 @@ void GameModel::SetSave(std::unique_ptr<SaveInfo> newSave, bool invertIncludePre
 			gameSave->authors["title"] = currentSave.saveInfo->name.ToUtf8();
 			gameSave->authors["description"] = currentSave.saveInfo->Description.ToUtf8();
 			gameSave->authors["published"] = (int)currentSave.saveInfo->Published;
-			gameSave->authors["date"] = int64_t(currentSave.saveInfo->updatedDate);
+			gameSave->authors["date"] = (Json::Value::UInt64)currentSave.saveInfo->updatedDate;
 			currentSave.saveInfo->SetGameSave(std::move(gameSave));
 		}
 		// This save was probably just created, and we didn't know the ID when creating it
 		// Update with the proper ID
-		else if (saveData->authors.Get("id", -1) == 0 || saveData->authors.Get("id", -1) == -1)
+		else if (saveData->authors.get("id", -1) == 0 || saveData->authors.get("id", -1) == -1)
 		{
 			auto gameSave = currentSave.saveInfo->TakeGameSave();
 			gameSave->authors["id"] = currentSave.saveInfo->id;

src/gui/save/LocalSaveActivity.cpp
@@ -105,13 +105,13 @@ void LocalSaveActivity::Save()
 void LocalSaveActivity::saveWrite(ByteString finalFilename)
 {
 	Platform::MakeDirectory(LOCAL_SAVE_DIR);
-	Bson localSaveInfo;
+	Json::Value localSaveInfo;
 	localSaveInfo["type"] = "localsave";
 	auto user = Client::Ref().GetAuthUser();
 	localSaveInfo["username"] = user ? user->Username : ByteString("");
 	localSaveInfo["title"] = finalFilename;
-	localSaveInfo["date"] = int64_t(time(nullptr));
-	Client::Ref().SaveAuthorInfo(localSaveInfo);
+	localSaveInfo["date"] = (Json::Value::UInt64)time(nullptr);
+	Client::Ref().SaveAuthorInfo(&localSaveInfo);
 	{
 		auto gameSave = save->TakeGameSave();
 		gameSave->authors = localSaveInfo;

commit 43de989cd7c86223453d5edb98783886bdc47e14

Fix ConvertJsonToBson mangling subobjects
Broken since 51f1afbf8633, where I failed at "merely rewriting old BSON API usage into new BSON API usage".

src/client/GameSave.cpp
@@ -2833,7 +2833,7 @@ static void ConvertJsonToBson(Bson &b, Json::Value j, int depth)
 			b[member.c_str()] = j[member].asInt64();
 		else if (j[member].isArray())
 		{
-			auto &array = b.Append(Bson::Type::arrayValue);
+			auto &array = (b[member] = Bson::Type::arrayValue);
 			std::set<int> saveIDs = std::set<int>();
 			int length = 0;
 			for (Json::Value::ArrayIndex i = 0; i < j[member].size(); i++)